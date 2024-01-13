f85e616a9e23e809df3a5ecd23373d520f145057
laksh <tjlakshmi10@gmail.com>
2024-01-13 12:15:18 +0530

    first commit

--- src/innovault_frontend/src/index.js
+++ src/innovault_frontend/src/index.js
@@ -1,19 +1,13 @@
-import { innovault_backend } from "../../declarations/innovault_backend";
+// innovault_frontend/index.js
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await innovault_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+// Example: Navigate to Register Page
+document.getElementById('registerLink').addEventListener('click', () => {
+    window.location.href = 'http://localhost:3000/register';
 });
+
+// Example: Navigate to Dashboard Page
+document.getElementById('dashboardLink').addEventListener('click', () => {
+    window.location.href = 'http://localhost:3000/dashboard';
+});
+
+// Other frontend logic goes here
